4c9c6c70f1d3327b5d926d9fca1499279b465eeb
SirSeko <43910503+SirSeko@users.noreply.github.com>
2021-02-12 11:54:46 +0200

    Pääsivu / Kirjautumissivu pohjat Vaate-luokka

--- Sovitushuone/Startup.cs
+++ Sovitushuone/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sovitushuone.Data;
 
 namespace Sovitushuone
 {
@@ -24,6 +26,9 @@ namespace Sovitushuone
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<SovitushuoneContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SovitushuoneContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +55,7 @@ namespace Sovitushuone
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Vaatteet}/{action=Index}/{id?}");
             });
         }
     }
